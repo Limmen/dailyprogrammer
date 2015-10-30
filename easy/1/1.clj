c560cf70472c1fc30f096c489f6a9b2b3079edd1
Limmen <kimham@kth.se>
2015-10-30 19:14:56 +0100

    .

---
+++ easy/1/1.clj
@@ -0,0 +1,13 @@
+(import '(java.util Scanner))
+(def scan (Scanner. *in*))
+
+(println "What is your Name?")
+(def Name (.nextLine scan))
+
+(println "What is your Age?")
+(def age (.nextLine scan))
+
+(println "What is your reddit username?")
+(def username (.nextLine scan))
+
+(println "Your name is " Name ", you are " age " years old, and your username is " username)
