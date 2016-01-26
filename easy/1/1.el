9bc048e64e7ddd2a1d7cdb02e40d26a2cdeb5dc4
Limmen <kimham@kth.se>
2016-01-26 18:37:32 +0100

    Add emacs-lisp solution

---
+++ easy/1/1.el
@@ -0,0 +1,26 @@
+;run with M-x eval-buffer
+
+(defun ask_name (arg)
+  "Prompt the user for a name"
+  (interactive "sEnter you name: ")
+  arg)
+
+(defun ask_age (arg)
+  "Prompt the user for age"
+  (interactive "nEnter you age: ")
+  arg)
+
+(defun ask_username (arg)
+  "Prompt the user for a username"
+  (interactive "sEnter you username: ")
+  arg)
+
+;setq is short for: (set 'name (call-interactively 'ask_name))
+;setq defines global variables
+;'name is short for (quote name) which means "do not evaluate name; let it be the atom name"
+(setq name (call-interactively 'ask_name))
+(setq age (call-interactively 'ask_age))
+(setq username (call-interactively 'ask_username))
+(message "Your name is: %s \n Your age is: %d \n Your username is: %s" name age username)
+
+
